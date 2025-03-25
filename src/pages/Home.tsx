248abdac846382b4726e60c683afc555b576c455
ReddIndiann <odaniel.ac@st.vvu.edu.gh>
2025-03-25 04:37:29 +0000

    asesss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import HeroSection from '../components/herosec';
 import MissionSection from '../components/MissionSection';
 import StateSection from '../components/StateSection';
-import StudentSection from '../components/StudentSection';
-import InstitutionSection from '../components/InstitutionSection';
+// import StudentSection from '../components/StudentSection';
+// import InstitutionSection from '../components/InstitutionSection';
 import TestimonialSection from '../components/TestimonialSection';
 import SchoolTestBanner from '../components/SchoolTestBanner';
 import BlogSection from '../components/BlogSection';
